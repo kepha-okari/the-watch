7a6e1f74c976d7f25bb3ae0aa69556ab6debbc7f
rkepha <kephaokari@gmail.com>
2018-02-09 23:11:31 +0300

    added the templates and urls

--- watch/watchapp/views.py
+++ watch/watchapp/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import Http404, HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
-# Create your views here.
+from wsgiref.util import FileWrapper
+import mimetypes
+from django.conf import settings
+import os
+
+@login_required(login_url='/accounts/login')
+def index(request):
+    # images = Image.get_images()
+    current_user = request.user
+
+    title = 'Timeline'
+
+    # user_info = Profile.objects.get(user=current_user.id)
+
+
+    return render(request, 'index.html', {"title": title, "user": current_user })
